9dfef85de4d3a5b3625d3dc2d42126974520f1d6
Samuel Galvão Elias <sgelias@outlook.com>
2024-03-10 04:12:18 -0300

    feat: implements the example data loading from the blutils mock data
    main

--- src/pages/@anon/Results/elements/ResultUpload.tsx
+++ src/pages/@anon/Results/elements/ResultUpload.tsx
@@ -1,7 +1,8 @@
 import { useForm, SubmitHandler } from "react-hook-form";
 import { BlutilsResult } from "@/types/BlutilsResult";
-import { FileInput, Button } from "flowbite-react";
+import { FileInput, Button, Spinner } from "flowbite-react";
 import { LandingPage } from "./LandingPage";
+import { useState } from "react";
 
 interface IFormInput {
   content: FileList;
@@ -12,7 +13,21 @@ interface Props {
 }
 
 export function ResultUpload({ resultSetter }: Props) {
+  const [isLoading, setIsLoading] = useState(false);
+
   const { register, handleSubmit } = useForm<IFormInput>();
+
+  const handleDownloadExample = () => {
+    setIsLoading(true);
+
+    fetch(
+      "https://raw.githubusercontent.com/LepistaBioinformatics/blutils/8c42f3e7bfe2d1e9de2038985e7c9a47625b6e78/test/mock/output/zymo-mock/blutils.consensus.json"
+    )
+      .then((response) => response.text())
+      .then((text) => resultSetter(JSON.parse(text)))
+      .catch((e) => console.error(e))
+      .finally(() => setIsLoading(false));
+  };
 
   const onSubmit: SubmitHandler<IFormInput> = async ({
     content,
@@ -29,21 +44,52 @@ export function ResultUpload({ resultSetter }: Props) {
   };
 
   return (
-    <div className="dark:text-gray-300 m-auto max-w-lg border p-5 my-32 rounded-xl">
-      <h1 className="text-3xl text-center font-bold mt-6 mb-12">
-        Results Explorer
-      </h1>
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="flex justify-around gap-3 my-5 border-t-2 border-t-gray-500 pt-5"
-      >
-        <FileInput id="file" {...register("content")} className="w-full" />
-        <Button type="submit">Submit</Button>
-      </form>
-      <p className="max-w-sm">
-        Use this field to load the JSON field containing the Blutils results
-      </p>
-      <LandingPage />
-    </div>
+    <>
+      <div className="dark:text-gray-300 m-auto max-w-lg border p-5 my-32 rounded-xl flex flex-col gap-16">
+        <div>
+          <h1 className="text-3xl text-center font-bold mt-6 mb-12">
+            Blutils Results Explorer
+          </h1>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex justify-around gap-3 my-5 border-t-2 border-t-gray-500 pt-5"
+          >
+            <FileInput id="file" {...register("content")} className="w-full" />
+            <Button type="submit">Submit</Button>
+          </form>
+          <p className="max-w-lg">
+            Load the JSON field generated after running the Blutils analysis
+          </p>
+          <LandingPage />
+        </div>
+
+        <div className="border-t-2 border-t-gray-500 my-5 pt-5">
+          <p className="my-3">
+            Or load{" "}
+            <a
+              href="https://github.com/LepistaBioinformatics/blutils/blob/8c42f3e7bfe2d1e9de2038985e7c9a47625b6e78/test/mock/output/zymo-mock/blutils.consensus.json"
+              className="text-blue-500 font-bold hover:underline"
+              target="_blank"
+              rel="noreferrer"
+            >
+              example data
+            </a>{" "}
+            from the{" "}
+            <a
+              href="https://github.com/LepistaBioinformatics/blutils"
+              className="text-blue-500 font-bold hover:underline"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Blutils
+            </a>{" "}
+            repository
+          </p>
+          <Button fullSized onClick={handleDownloadExample}>
+            {isLoading ? <Spinner /> : "Load example"}
+          </Button>
+        </div>
+      </div>
+    </>
   );
 }
